5b6471671b78a9e94b9821172323a3b33eeb8e59
Andrew Downie <andrew_dev@outlook.com>
2017-04-01 19:28:32 -0400

    Worked on a1p2 doc a little. Added putting the timing of a program run into a text file labeled by the algorithm ran, and the time the program finished. The contents of the file are how long it took to run. I can now run my algos overnight, and collect the data into a chart.

--- a1p1.py
+++ a1p1.py
@@ -15,6 +15,11 @@ import a1p1_code
 from imageIO import *
 import imageIO
 
+from datetime import date, datetime
+import time 
+
+
+
 #####
 #####               Get command line args 
 #####
@@ -32,6 +37,7 @@ print('')
 img_r, img_g, img_b = imageIO.imread_colour(inputImage)
 
 
+
 #####
 #####               Process the image
 #####
@@ -39,12 +45,12 @@ if algo == 'morph_toggleCE':
     new_r, new_g, new_b = a1p1_code.morph_toggleCE(img_r, img_g, img_b)
 
     
-    new_r, new_g, new_b = a1p1_code.morph_toggleCE(new_r, new_g, new_b)
-    new_r, new_g, new_b = a1p1_code.morph_toggleCE(new_r, new_g, new_b)
+    #new_r, new_g, new_b = a1p1_code.morph_toggleCE(new_r, new_g, new_b)
+    #new_r, new_g, new_b = a1p1_code.morph_toggleCE(new_r, new_g, new_b)
 
 elif algo == 'morph_CE':
     new_r, new_g, new_b = a1p1_code.morph_CE(img_r, img_g, img_b)
-    new_r, new_g, new_b = a1p1_code.morph_CE(new_r, new_g, new_b)
+    #new_r, new_g, new_b = a1p1_code.morph_CE(new_r, new_g, new_b)
 
 elif algo == 'histhyper':
     new_r, new_g, new_b = a1p1_code.histhyper(img_r, img_g, img_b)
@@ -64,4 +70,8 @@ else:
 print("\nsaving image...\n")
 imageIO.imwrite_colour(outputImage, new_r, new_g, new_b)
 print("\nDone saving image...\nexiting...\n")
+
+f = open(algo + " - " + str(datetime.now()).replace(":", ".") + ".txt", 'w')
+f.write(str(time.clock())) 
+f.close()  
                         
